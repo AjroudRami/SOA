3f2ed830de2bd63d55a0fa4373bf69eac29592bf
Ajroud Rami <ajroud.rami@hotmail.fr>
2017-10-08 01:31:15 +0200

    test(flightservice) Add all tests

--- tests/acceptation/src/test/java/scenarios/FlightStepDefinition.java
+++ tests/acceptation/src/test/java/scenarios/FlightStepDefinition.java
@@ -1,15 +1,20 @@
 package scenarios;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.media.jfxmedia.logging.Logger;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.apache.cxf.jaxrs.client.WebClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.Assert;
 
 import javax.ws.rs.core.MediaType;
 
 import java.util.Arrays;
+import java.util.logging.Level;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -19,9 +24,11 @@ public class FlightStepDefinition {
     private static final String ROOM_COST_FIELD = "roomCost";
 
     private static class ServiceRequest {
+
         private static class Filter{
             private String name;
             private String value;
+
             public Filter(String name, String value) {
                 this.name = name;
                 this.value = value;
@@ -71,12 +78,12 @@ public class FlightStepDefinition {
         requestDetail.destination = arrivalAirport;
     }
 
-    @Given("^a departure date (\\d+)")
+    @Given("^a departure date (\\d+)$")
     public void departureDate(int date) {
         requestDetail.depTimestamp = date;
     }
 
-    @Given("^ordering by (price|duration)")
+    @Given("^ordering by (price|duration)$")
     public void orderingBy(String ordering) {
         requestDetail.ordering = ordering;
     }
@@ -87,25 +94,94 @@ public class FlightStepDefinition {
         requestDetail.AddFilter(fl);
     }
 
-    @Given("^a filter (.*) with value (.*)$ ")
+    @Given("^a filter ([a-zA-Z]+(?:_[a-zA-Z]+)*) with value ([a-zA-Z0-9]*)$")
     public void addFilter(String filterName, String filterValue) {
         ServiceRequest.Filter fl = new ServiceRequest.Filter(filterName, filterValue);
         requestDetail.AddFilter(fl);
     }
+
+
+    @When("^the flight research is sent$")
+    public void send(){
+        try {
+            sendRequest();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Then("^the flights are filtered : ([a-zA-Z]+(?:_[a-zA-Z]+)*) ([a-zA-Z0-9]*)$")
+    public void theFlightsAreFiltered(String filterName, String filterValue) {
+        if (answer.length() == 0) {
+            return;
+        }
+        if ("direct".equals(filterName)){
+            for(int i = 0; i < answer.length(); i ++) {
+                int nbOfFlights = answer.getJSONObject(i).getInt("number_of_flights");
+                Assert.assertEquals(1, nbOfFlights);
+            }
+        } else if ("max_duration".equals(filterName)) {
+            for(int i = 0; i < answer.length(); i ++) {
+                int currentArrival = answer.getJSONObject(i).getInt("arrival");
+                int currentDeparture = answer.getJSONObject(i).getInt("departure");
+                int currentDuration = currentArrival - currentDeparture;
+
+                Assert.assertTrue(currentDuration < Integer.parseInt(filterValue));
+            }
+        }
+    }
+
+    @Then("^flights are suggested$")
+    public void flightsAreSuggested(){
+        Assert.assertFalse(answer.length() == 0);
+    }
+
+    @Then("^the flights are ordered by (price|duration)$")
+    public void theFlightsAreOrderedBy(String ordering) {
+        if (answer.length() == 0) {
+            return;
+        }
+        if ("price".equals(ordering)) {
+            double previousPrice = answer.getJSONObject(0).getDouble("price");
+            for (int i = 1; i < answer.length(); i++) {
+                double currentPrice = answer.getJSONObject(i).getDouble("price");
+                assertFalse(currentPrice < previousPrice);
+                previousPrice = currentPrice;
+            }
+        }else if ("duration".equals(ordering)) {
+            int previousArrival = answer.getJSONObject(0).getInt("arrival");
+            int previousDeparture = answer.getJSONObject(0).getInt("departure");
+            int previousDuration = previousArrival - previousDeparture;
+            for (int i = 1; i < answer.length(); i++) {
+                int currentArrival = answer.getJSONObject(i).getInt("arrival");
+                int currentDeparture = answer.getJSONObject(i).getInt("departure");
+                int currentDuration = currentArrival - currentDeparture;
+                assertFalse(currentDuration < previousDuration);
+                previousDuration = currentDuration;
+            }
+        }
+    }
+
 
     private String getUrl() {
         String url = "http://" + host + ":" + port + "/tcs-hotel-service/hotels?";
         return url;
     }
 
-    private void sendRequest() {
+    private void sendRequest() throws JsonProcessingException {
         String url = getUrl();
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        //Object to JSON in String
+        String jsonInString = mapper.writeValueAsString(requestDetail);
 
         String body = (String) WebClient.create(url)
                 .accept(MediaType.APPLICATION_JSON_TYPE)
                 .header("Content-Type", MediaType.APPLICATION_JSON)
-                .get()
+                .post(jsonInString)
                 .readEntity(String.class);
+        Logger.logMsg(Logger.INFO, jsonInString);
 
         answer = new JSONArray(body);
     }
--- tests/acceptation/src/test/resources/scenarios/flights.feature
+++ tests/acceptation/src/test/resources/scenarios/flights.feature
@@ -4,48 +4,48 @@ Feature: Flight research
 
   Scenario: Simple research
     Given a research for a flight booking
-    And a departure airport located in Nice
-    And an arrival airport located in Paris
-    And a departure date 12345
-    When the research is sent
+    And a departure airport located in Paris
+    And an arrival airport located in New-York
+    And a departure date 0710171030
+    When the flight research is sent
     Then flights are suggested
 
   Scenario: Research ordered by prices
     Given a research for a flight booking
-    And a departure airport located in Nice
-    And an arrival airport located in Paris
-    And a departure date 12345
+    And a departure airport located in Paris
+    And an arrival airport located in New-York
+    And a departure date 0710171030
     And ordering by price
-    When the research is sent
-    Then hotels are suggested
-    And the flights are order by price
+    When the flight research is sent
+    Then flights are suggested
+    And the flights are ordered by price
 
   Scenario: Research ordered by duration
     Given a research for a flight booking
-    And a departure airport located in Nice
-    And an arrival airport located in Paris
+    And a departure airport located in Paris
+    And an arrival airport located in New-York
     And a departure date 12345
     And ordering by duration
-    When the research is sent
-    Then hotels are suggested
-    And the flights are order by duration
+    When the flight research is sent
+    Then flights are suggested
+    And the flights are ordered by duration
 
   Scenario: Research filtered by direct flights only
     Given a research for a flight booking
-    And a departure airport located in Nice
-    And an arrival airport located in Paris
+    And a departure airport located in Paris
+    And an arrival airport located in New-York
     And a departure date 12345
     And a simple filter direct
-    When the research is sent
-    Then hotels are suggested
-    And the flights are filtered : direct
+    When the flight research is sent
+    Then flights are suggested
+    And the flights are filtered : direct no
 
   Scenario: Research filtered by max duration
     Given a research for a flight booking
-    And a departure airport located in Nice
-    And an arrival airport located in Paris
+    And a departure airport located in Paris
+    And an arrival airport located in New-York
     And a departure date 12345
     And a filter max_duration with value 1000
-    When the research is sent
-    Then hotels are suggested
+    When the flight research is sent
+    Then flights are suggested
     And the flights are filtered : max_duration 1000
