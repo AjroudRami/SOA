a31736a1123eac13ff955937c2fa97b9e8792f9d
danial <azswad20@gmail.com>
2017-10-27 00:19:35 +0200

    cheapest hotel bus implementation

---
+++ integration/flows/src/main/java/fr/unice/polytech/esb/flows/hotels/CheapestHotelExternal.java
@@ -0,0 +1,11 @@
+package fr.unice.polytech.esb.flows.hotels;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class CheapestHotelExternal extends RouteBuilder{
+
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
---
+++ integration/flows/src/main/java/fr/unice/polytech/esb/flows/hotels/CheapestHotelInternal.java
@@ -0,0 +1,11 @@
+package fr.unice.polytech.esb.flows.hotels;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class CheapestHotelInternal extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
