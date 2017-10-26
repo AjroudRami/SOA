b90230be21df0387df92db1fa816c082c298278c
danial <azswad20@gmail.com>
2017-10-26 22:01:23 +0200

    Merge branch 'develop' of /Users/danial/Documents/Polytech/SOA/polytech-soa with conflicts.

---
+++ integration/flows/src/main/java/fr/unice/polytech/esb/flows/reports/ReportManagement.java
@@ -0,0 +1,10 @@
+package fr.unice.polytech.esb.flows.reports;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class ReportManagement extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
--- integration/flows/src/main/java/fr/unice/polytech/esb/flows/utils/Endpoints.java
+++ integration/flows/src/main/java/fr/unice/polytech/esb/flows/utils/Endpoints.java
@@ -3,7 +3,7 @@ package fr.unice.polytech.esb.flows.utils;
 public class Endpoints {
 
     // Servlet endpoints.
-    public static final String SEARCH_CHEAPEST_FLIGHT = "activemq:bus-cheapest-flight";
+    public static final String SEARCH_CHEAPEST_FLIGHT = "servlet://bus-cheapest-flight";
 
     // Error handling.
     public static final String DEATH_POOL = "activemq:global:dead";
@@ -16,6 +16,8 @@ public class Endpoints {
     public static final String SEARCH_IN_EXTERNAL_FLIGHT_SERVICE = "direct:external-flight-service";
 
     // External partners.
-    public static final String INTERNAL_FLIGHTS_ENDPOINT = "http://internal-flights:8080/tcs-service-flights/flights/";
-    public static final String EXTERNAL_FLIGHTS_ENDPOINT = "http://external-flights:8081/tta-service-rpc/FlightBookingService";
+    public static final String INTERNAL_FLIGHTS_ENDPOINT = "http://localhost:8080/tcs-service-flights/flights/";
+    public static final String EXTERNAL_FLIGHTS_ENDPOINT = "http://localhost:8081/tta-service-rpc/FlightBookingService";
+    public static final String TRAVEL_REPORT_ENDPOINT = "";
+    public static final String BUSINESS_TRAVEL_ENDPOINT = "";
 }
