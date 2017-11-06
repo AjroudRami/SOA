45afab80427777e561617b7a26b78771040927b0
Ajroud Rami <ajroud.rami@hotmail.fr>
2017-11-06 17:13:50 +0100

    Fix build script
    Building only the needed external services

--- build-external-services.sh
+++ build-external-services.sh
@@ -2,23 +2,23 @@
 
 echo ------ Building external services ------
 
-cd external-services/
+cd external-services
 
-echo Building Team 3 services
-cd team-3
-bash build.sh
-cd ..
+echo Building Team 3 Flight service
+cd team-3/services/rpc/
+mvn clean package
+cd ../../..
+
+pwd
 
 echo Building Team 5 services
-cd team-5
-bash build.sh
-cd ..
+cd team-5/services/hotelRPC/
+mvn clean package
+cd ../../..
 
 echo Building Team 8 services
-cd team-8
-bash build.sh
-cd ..
-
-cd ..
+cd team-8/services/car/
+mvn clean package
+cd ../../../..
 
 echo ----- End of external services building -----
