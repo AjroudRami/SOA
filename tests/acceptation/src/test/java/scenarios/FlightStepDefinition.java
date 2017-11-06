023cd5694cef9516aedcb98bb5c0e02df60fb8b8
Ajroud Rami <ajroud.rami@hotmail.fr>
2017-11-06 15:53:14 +0100

    Debug compilation problem

--- tests/acceptation/src/test/java/scenarios/FlightStepDefinition.java
+++ tests/acceptation/src/test/java/scenarios/FlightStepDefinition.java
@@ -10,12 +10,117 @@ import org.apache.cxf.jaxrs.client.WebClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Assert;
-import request.Filter;
-import request.ServiceRequest;
 import javax.ws.rs.core.MediaType;
 import static org.junit.Assert.assertFalse;
 
 public class FlightStepDefinition {
+
+    public class Filter{
+        private String name;
+        private String[] args;
+
+        public Filter(String name, String[] args) {
+            this.name = name;
+            this.args = args;
+        }
+
+        public Filter(){}
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String[] getArgs() {
+            return args;
+        }
+
+        public void setArgs(String[] args) {
+            this.args = args;
+        }
+    }
+
+    public class ServiceRequest {
+
+        private String event;
+        private String destination;
+        private String departure;
+        private int departureTimeStamp;
+        private Filter[] filterBy;
+        private String orderBy;
+
+        public ServiceRequest(){
+        }
+
+        public ServiceRequest(String event, String destination, String departure, int departureTimeStamp, Filter[] filterBy, String orderBy) {
+            this.event = event;
+            this.destination = destination;
+            this.departure = departure;
+            this.departureTimeStamp = departureTimeStamp;
+            this.filterBy = filterBy;
+            this.orderBy = orderBy;
+        }
+
+        public String getEvent() {
+            return event;
+        }
+
+        public void setEvent(String event) {
+            this.event = event;
+        }
+
+        public String getDestination() {
+            return destination;
+        }
+
+        public void setDestination(String destination) {
+            this.destination = destination;
+        }
+
+        public String getDeparture() {
+            return departure;
+        }
+
+        public void setDeparture(String departure) {
+            this.departure = departure;
+        }
+
+        public int getDepartureTimeStamp() {
+            return departureTimeStamp;
+        }
+
+        public void setDepartureTimeStamp(int departureTimeStamp) {
+            this.departureTimeStamp = departureTimeStamp;
+        }
+
+        public Filter[] getFilterBy() {
+            return filterBy;
+        }
+
+        public void setFilterBy(Filter[] filterBy) {
+            this.filterBy = filterBy;
+        }
+
+        public String getOrderBy() {
+            return orderBy;
+        }
+
+        public void setOrderBy(String orderBy) {
+            this.orderBy = orderBy;
+        }
+
+        public void AddFilter(Filter filter){
+            if (this.filterBy == null) {
+                filterBy = new Filter[0];
+            }
+            Filter[] newFilters = Arrays.copyOf(filterBy, filterBy.length + 1);
+            newFilters[newFilters.length - 1] = filter;
+            this.filterBy = newFilters;
+        }
+    }
 
     private String host = "localhost";
     private int port = 8080;
