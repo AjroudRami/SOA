63ac247c86e6ec5a496e9c0a2aa5f1961fdc0bb6
scipio3000 <gunther@gameslabs.net>
2017-11-12 16:05:46 +0100

    feat: update cheapest car and hotel script

--- scenarios/list/cheapest-car.sh
+++ scenarios/list/cheapest-car.sh
@@ -3,13 +3,13 @@
 echo "Getting cheapest car"
 
 result=$(curl -X POST \
-  http://flow:8181/tars/flights/search \
+  http://flow:8181/tars/cars/search \
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
   -d '{
-    "to": "New-York",
-    "from": "Paris",
-    "departure": 710170930
+    "city": "New-York",
+    "dateFrom": "2018-10-2",
+    "dateTo": "2018-10-2"
 }' -s)
 
 price=$(echo ${result} | jq .price)
--- scenarios/list/cheapest-hotel.sh
+++ scenarios/list/cheapest-hotel.sh
@@ -3,13 +3,12 @@
 echo "Getting cheapest hotel"
 
 result=$(curl -X POST \
-  http://flow:8181/tars/flights/search \
+  http://flow:8181/tars/hotels/search \
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
   -d '{
-    "to": "New-York",
-    "from": "Paris",
-    "departure": 710170930
+    "destination": "Liudu",
+    "timestamp": 10923443534
 }' -s)
 
 price=$(echo ${result} | jq .price)
