e7261723d60237fe5c57e1395d55a08b050fc47c
danial <azswad20@gmail.com>
2017-11-12 17:03:11 +0100

    Business/Travel reports scenario

--- scenarios/list/create-business-travel-and-validate-with-expense.sh
+++ scenarios/list/create-business-travel-and-validate-with-expense.sh
@@ -43,18 +43,14 @@ echo Approved the business travel created the travel report of id : ${trId}
 
 echo We will now add some expenses to our travel report
 
-for i in $(seq 1 5)
-do
-    toAdd=$(( ( RANDOM % 5000 )  + 5000 ))
-    result=$(curl -X POST \
-      http://flow:8181/tars/travel-report/expenses/ \
-      -H 'cache-control: no-cache' \
-      -H 'content-type: application/json' \
-      -H 'postman-token: 5b98265b-a072-820a-c8f4-08667d975cb8' \
-      -d '{"id": '${trId}', "expenses": [{"amount": '${toAdd}', "description": "super expense"}]}' -s)
-    amount=$(echo ${result} | jq .totalAmount)
-    echo We added an expense, the total amount of our travel report is now : ${amount}
-done
+result=$(curl -X POST \
+  http://flow:8181/tars/travel-report/expenses/ \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'postman-token: 5b98265b-a072-820a-c8f4-08667d975cb8' \
+  -d '{"id": '${trId}', "expenses": [{"amount": 250, "description": "my expense"}]}' -s)
+amount=$(echo ${result} | jq .totalAmount)
+echo We added an expense, the total amount of our travel report is now : ${amount}
 
 
 echo We will now end the travel report and maybe have our money refund
@@ -68,23 +64,3 @@ result=$(curl -X POST \
 status=$(echo ${result} | jq .status)
 echo Our business travel now has the status : ${status}
 
-echo We will now add some explaination to our travel report
-result=$(curl -X POST \
-  http://localhost:8181/tars/travel-report/explain/ \
-  -H 'cache-control: no-cache' \
-  -H 'content-type: application/json' \
-  -H 'postman-token: 08af9196-3167-d643-8213-89526fc2136c' \
-  -d '{"id": '${trId}', "explaination":"There was a strike, I had to book another flight back home")}')
-
-status=$(echo ${result} | jq .explaination)
-
-echo We will now validate the travel report to get the money refund
-result=$(curl -X POST \
-  http://flow:8181/tars/travel-report/approve/ \
-  -H 'cache-control: no-cache' \
-  -H 'content-type: application/json' \
-  -H 'postman-token: 5b98265b-a072-820a-c8f4-08667d975cb8' \
-  -d '{"id": '${trId}'}' -s)
-
-status=$(echo ${result} | jq .status)
-echo Our business travel now has the status : ${status}
